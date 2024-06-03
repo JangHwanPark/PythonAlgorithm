19043a1107927c6767691e68dfdf86b3e1415c66
user <wkd5878@gmail.com>
2024-06-04 05:02:36 +0900

    수열, 등차수열, 등비수열

--- CoTest/exam02.py
+++ CoTest/exam02.py
@@ -4,7 +4,7 @@ print(sqe)
 
 
 # 등차 수열
-def arithmetic_sqe(a,d,n):
+def arithmetic_sqe(a, d, n):
     return [a + d * i for i in range(n)]
 
 
@@ -14,6 +14,33 @@ sqe3 = arithmetic_sqe(2, 2, 10)
 print(sqe1)
 print(sqe2)
 print(sqe3)
+
+
+# 등차 수열의 합
+def arithmetic_sum(a, d, n):
+    l = a + (n - 1) * d
+    return n * (a + l) // 2
+
+
+sqe_sum1 = arithmetic_sum(1, 1, 10)
+sqe_sum2 = arithmetic_sum(1, 2, 6)
+sqe_sum3 = arithmetic_sum(2, 2, 10)
+print(sqe_sum1)
+print(sqe_sum2)
+print(sqe_sum3)
+
+
+# 리턴값으로 합 구하기
+def arithmetic_return_sum(sequence):
+    return sum(sequence)
+
+
+return_sum1 = arithmetic_return_sum(sqe1)
+return_sum2 = arithmetic_return_sum(sqe2)
+return_sum3 = arithmetic_return_sum(sqe3)
+print(return_sum1)
+print(return_sum2)
+print(return_sum3)
 
 
 # 등비 수열
